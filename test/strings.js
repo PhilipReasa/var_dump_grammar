1e8cafbbbb4c3b949b6525e8911b3f51bfe7b699
Rece <PhilipReasa@gmail.com>
2017-02-18 16:07:51 -0800

    completed string tests

--- test/strings.js
+++ test/strings.js
@@ -22,46 +22,192 @@ describe('[STRING]', () => {
     })
 
     it('Handles the single letter string', () => {
+        const dump = dumps['string2']
 
+        expect(parse(dump)).to.be.deep.equal({
+            type: 'array',
+            count: 1,
+            values: [
+                {
+                    key: 0,
+                    value: {
+                        type: 'string',
+                        length: 1,
+                        value: "a"
+                    }
+                }
+            ]
+        })
     })
 
     it('Handles the string with just a double quote', () => {
+        const dump = dumps['string3']
 
+        expect(parse(dump)).to.be.deep.equal({
+            type: 'array',
+            count: 1,
+            values: [
+                {
+                    key: 0,
+                    value: {
+                        type: 'string',
+                        length: 1,
+                        value: "\""
+                    }
+                }
+            ]
+        })
     })
 
     it('Handles the string with just a single quote', () => {
+        const dump = dumps['string4']
 
+        expect(parse(dump)).to.be.deep.equal({
+            type: 'array',
+            count: 1,
+            values: [
+                {
+                    key: 0,
+                    value: {
+                        type: 'string',
+                        length: 1,
+                        value: "\'"
+                    }
+                }
+            ]
+        })
     })
 
     it('Handles a string that contains a link', () => {
+        const dump = dumps['string5']
 
+        expect(parse(dump)).to.be.deep.equal({
+            type: 'array',
+            count: 1,
+            values: [
+                {
+                    key: 0,
+                    value: {
+                        type: 'string',
+                        length: 49,
+                        value: "<a href='http://sampleLink.com/#/sample'>link</a>"
+                    }
+                }
+            ]
+        })
     })
 
     it('Handles a string with a new line in the middle', () => {
+        const dump = dumps['string6']
 
+        expect(parse(dump)).to.be.deep.equal({
+            type: 'array',
+            count: 1,
+            values: [
+                {
+                    key: 0,
+                    value: {
+                        type: 'string',
+                        length: 24,
+                        value: "first-word\n second-word"
+                    }
+                }
+            ]
+        })
     })
 
     it('Handles a string with a new line at the end', () => {
+        const dump = dumps['string7']
 
+        expect(parse(dump)).to.be.deep.equal({
+            type: 'array',
+            count: 1,
+            values: [
+                {
+                    key: 0,
+                    value: {
+                        type: 'string',
+                        length: 12,
+                        value: "first-word \n"
+                    }
+                }
+            ]
+        })
     })
 
     it('Handles a single new line', () => {
+        const dump = dumps['string8']
 
+        expect(parse(dump)).to.be.deep.equal({
+            type: 'array',
+            count: 1,
+            values: [
+                {
+                    key: 0,
+                    value: {
+                        type: 'string',
+                        length: 1,
+                        value: "\n"
+                    }
+                }
+            ]
+        })
     })
 
     it('Handles a new line followed by a space', () => {
+        const dump = dumps['string9']
 
+        expect(parse(dump)).to.be.deep.equal({
+            type: 'array',
+            count: 1,
+            values: [
+                {
+                    key: 0,
+                    value: {
+                        type: 'string',
+                        length: 2,
+                        value: "\n "
+                    }
+                }
+            ]
+        })
     })
 
     it('Handles really complicated jabbering', () => {
+        const dump = dumps['string10']
 
-    })
-
-    it('Handles a new line as the first character', () => {
-
+        expect(parse(dump)).to.be.deep.equal({
+            type: 'array',
+            count: 1,
+            values: [
+                {
+                    key: 0,
+                    value: {
+                        type: 'string',
+                        length: 37,
+                        value: "\"\n\n\"\"\"\nthere should be something here"
+                    }
+                }
+            ]
+        })
     })
 
     it('Has the wrong length for the string (yes this is possible with non-standard chars)', () => {
+        const dump = dumps['string11']
 
+        expect(parse(dump)).to.be.deep.equal({
+            type: 'array',
+            count: 1,
+            values: [
+                {
+                    key: 0,
+                    value: {
+                        type: 'string',
+                        length: 36,
+                        value: "мультфильм, комедия"
+                    }
+                }
+            ]
+        })
     })
 })
